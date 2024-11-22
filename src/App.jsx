90db01cb6e070830ca53b1355b153524ce9c0289
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2024-11-22 14:57:27 +0700

    DONE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ToDoList from "./ToDoList.jsx"
+
+
+function App() {
+  
+
+  return (
+      <div className="min-h-screen bg-slate-800">
+        <ToDoList />
+      </div>
+  )
+}
+
+export default App
